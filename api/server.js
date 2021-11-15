85071b3cc41b559cdd7b16861bdaea0fd21d9e82
Brandon May <battleb2014@gmail.com>
2021-11-15 15:00:36 -0500

    heroku init commit

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
+server.use(cors());
 
 server.use((err, req, res, next) => {
     res.status(500).json({
