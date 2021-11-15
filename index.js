5dbac5178c5432a4a58c300e900d7b2523af398d
Brandon May <battleb2014@gmail.com>
2021-11-15 11:40:05 -0500

    dependencies installed

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv');
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 9000;
+server.listen(port, () => {
+    console.log(`-- Listening on port ${port} --`)
+})
