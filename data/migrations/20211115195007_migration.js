bf6c823d3278d1b0074f1802d29208af77327266
Amira May <amiramay@Amiras-MacBook-Pro.local>
2021-11-16 17:22:39 -0500

    migrations built

--- data/migrations/20211115195007_migration.js
+++ data/migrations/20211115195007_migration.js
@@ -40,45 +40,40 @@ exports.up = function(knex) {
       .notNullable()
       .unique()
       .index();
-    table.string('instructor_name').notNullable();
-    table.string('type')
-      .notNullable();
+    table.integer('instructor_id')
+      .notNullable()
+      .unsigned()
+      .references('instructor_id')
+      .inTable('users')
+      .onUpdate(CASCADE)
+      .onDelete(CASCADE)
+    table.integer('catagory_id')
+      .notNullable()
+      .unsigned()
+      .references('catagory_id')
+      .inTable('catagories')
+      .onUpdate(CASCADE)
+      .onDelete(CASCADE)
     table.string('intensity')
       .notNullable()
-      .index();
     table.string('date')
       .notNullable()
-      .index();
-    table.string('location')
+    table.string('address', 128)
       .notNullable()
-      .index();
+    table.strig('city')
+      .notNullable()
+    table.strig('state')
+      .notNullable()
+    table.strig('zip')
+      .notNullable()
     table.integer('max_size')
       .notNullable();
     table.float('duration')
-      .notNullable();
-    table.bool('signedUp')
-      .defaultTo('false');
+      .notNullable()
     table.integer('current_members')
       .defaultTo(0);
   })
 
-  .createTable('instructors', table => {
-    table.increments();
-    table.integer('user_id')
-      .notNullable()
-      .unique()
-      .references('user_id')
-      .inTable('users')
-      .onDelete('RESTRICT')
-      .onUpdate('CASCADE')
-    table.integer('class_id')
-      .notNullable()
-      .unique()
-      .references('class_id')
-      .inTable('classes')
-      .onDelete('RESTRICT')
-      .onUpdate('CASCADE')
-  })
     .createTable('roles', roles => {
         roles.increments('role_id')
         roles.string('role').notNullable().unique()
@@ -90,15 +85,15 @@ exports.up = function(knex) {
       table.integer('user_id')
         .notNullable()
         .references('user_id')
-        .inTable('user')
-        .onDelete('RESTRICT')
+        .inTable('users')
+        .onDelete('CASCADE')
         .onUpdate('CASCADE')
   
       table.integer('class_id')
         .notNullable()
         .references('id')
         .inTable('class')
-        .onDelete('RESTRICT')
+        .onDelete('CASCADE')
         .onUpdate('CASCADE')
     })
 }
